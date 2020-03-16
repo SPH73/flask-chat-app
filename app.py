42bbc8c1886ee01d3c2c8c13b1360ca7b353e390
Sue Holder <susanpholder@gmail.com>
2020-03-16 18:08:05 +0000

    add function to store messages in a list update user function

--- app.py
+++ app.py
@@ -2,19 +2,28 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+messages = []
+
+
+def add_message(username, message):
+    messages.append("{0}: {1}".format(username, message))
+
 
 @app.route('/')
 def index():
-    return "<h1>Hello There!</h1>"
+    """ main page with instructions """
+    return "To send a message use /username/message"
 
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """ Display username in app in the chat page"""
+    return "Welcome, {0} ".format(username, messages)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
+    """ Create a new message and redirect back to the chat page"""
     return "{0}: {1}".format(username, message)
 
 
